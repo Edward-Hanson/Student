2fd3f4cb82f88eae55ea748d7ca87ed3aaf6513b
Edward Hanson <ehanson787@gmail.com>
2023-11-13 07:31:03 +0000

    3rd phase

--- pairings/urls.py
+++ pairings/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 
-from .views import pair_students
+from .views import get_interest
 
 urlpatterns = [
-    path('pairs/',pair_students,name='pairs'),
+    path('get/interest',get_interest,name='get_interest'),
     path('interests',TemplateView.as_view(template_name='interest.html'),name='interests'),
 ]
