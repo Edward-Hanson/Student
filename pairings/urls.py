28cb5b885133f1d70a483882bb24a431348ec54a
Edward Hanson <ehanson787@gmail.com>
2023-11-14 07:37:44 +0000

    4th phase

--- pairings/urls.py
+++ pairings/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 
 
-from .views import get_interest
+from .views import get_interest, pairs
 
 urlpatterns = [
+    path('pairs',pairs,name='pairs'),
     path('get/interest',get_interest,name='get_interest'),
     path('interests',TemplateView.as_view(template_name='interest.html'),name='interests'),
 ]
--- pairings/views.py
+++ pairings/views.py
@@ -5,37 +5,55 @@ from django.shortcuts import redirect
 from django.contrib import messages
 from django.http import HttpResponse
 import json
+from django.contrib.auth.decorators import login_required
 
 from accounts.models import CustomUser, Student_Interest
 from .models import Room
 
-
+@login_required
 def get_interest(request):
     user=request.user
-    selected_interests= request.POST.get('selected_interests')
-    my_list = json.loads(selected_interests)
-    for item in my_list:
-        interest = Student_Interest.objects.create(interest=item,student=user)
-    return redirect('pairs')
+    if not user.room:
+        selected_interests= request.POST.get('selected_interests')
+        my_list = json.loads(selected_interests)
+        for item in my_list:
+            interest = Student_Interest.objects.create(interest=item,student=user)
+        print("get interest")
+        return redirect('pairs')
+    print("get_interest_failed")
+    return redirect("home")
 
+def send_mail(user):
+    recipient_email = user.student_mail
+
+    email = EmailMessage(
+    subject='KINDRED HOSTELS',
+    body=f'You have successfully booked a room, Check your portal for more information',
+    from_email="ehanson787@gmail.com",
+    to=[recipient_email],
+             )
+    email.send()
+                      
+@login_required
 def pairs(request):
     user= request.user
-    user_interest = user.interests.all()
+    user_interests = user.interests.all()
     Rooms = Room.objects.all()
     
     if not user.room:
         for room in Rooms:
             if room.students.count()>=4:
                 continue
-            
             else:
                 if room.room_interest=="":
                     room.students.add(user)
                     interests = ''
+                    user_interests= list(user.interests.values_list('interest',flat=True))
                     for item in user_interests:
                         interests = interests + "|" +item
                     room.room_interest = interests
                     room.save()
+                    # send_mail(user)
                     return render(request,'pairs.html',{'room':room,'user':user})
                 else:
                     user_interests= list(user.interests.values_list('interest',flat=True))
@@ -50,22 +68,7 @@ def pairs(request):
                                 interest = interest + "|" + item
                             room.room_interest = interest
                             room.save()
+                        return render(request,'pairs.html',{'room':room,'user':user})
+    
                     continue
-                        
-                        #send mail
-                        recipient_email = user.student_mail
-
-                        email = EmailMessage(
-                        subject='KINDRED HOSTELS',
-                        body=f'You have successfully booked a room, Check your portal for more information',
-                        from_email="ehanson787@gmail.com",
-                        to=[recipient_email],
-                                         )
-                        email.attach_file()
-                        email.send()
-                        return render(request,'pairs.html',{'room_interest':room_interests,'room':room,'user':user})
-        else:
-            return redirect('interests')
-            messages.info(request,"You already have a room, you can not book more room")
-    return redirect('interests')
-
+    return redirect('home')
